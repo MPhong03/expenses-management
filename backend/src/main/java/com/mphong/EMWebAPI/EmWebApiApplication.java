948be5a820349a4dc249656bbfe9cf99f261ea2b
MPhong03 <dangminhphong912@gmail.com>
2024-06-07 15:42:33 +0700

    INIT PROJECT

---
+++ backend/src/main/java/com/mphong/EMWebAPI/EmWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.mphong.EMWebAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmWebApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mphong/EMWebAPI/EmWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mphong.EMWebAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
