4ca67d2c8dabb66d6ca66c54f747dc238da93800
MPhong03 <dangminhphong912@gmail.com>
2024-06-08 14:25:04 +0700

    build(env): create .env

--- backend/src/main/java/com/mphong/EMWebAPI/EmWebApiApplication.java
+++ backend/src/main/java/com/mphong/EMWebAPI/EmWebApiApplication.java
@@ -1,12 +1,23 @@
 package com.mphong.EMWebAPI;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class EmWebApiApplication {
 
 	public static void main(String[] args) {
+		// LOAD ENVIRONMENT
+		Dotenv env = Dotenv.load();
+		System.setProperty("MYSQL_HOST", Objects.requireNonNull(env.get("MYSQL_HOST")));
+		System.setProperty("MYSQL_PORT", Objects.requireNonNull(env.get("MYSQL_PORT")));
+		System.setProperty("MYSQL_DB", Objects.requireNonNull(env.get("MYSQL_DB")));
+		System.setProperty("MYSQL_USER", Objects.requireNonNull(env.get("MYSQL_USER")));
+		System.setProperty("MYSQL_PASSWORD", Objects.requireNonNull(env.get("MYSQL_PASSWORD")));
+
 		SpringApplication.run(EmWebApiApplication.class, args);
 	}
 
